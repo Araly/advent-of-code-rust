26fc737b5c099715d9d7150479eacca0c099c5f1
araly <araly.isaac@proton.me>
2024-12-13 17:02:21 +0100

    day 12 part 1

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,26 +1,27 @@
-use std::collections::HashMap;
-
 use array2d::{Array2D, Error};
 
 advent_of_code::solution!(12);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let (plots, regions) = match parse_farm(input) {
+    let mut plots = match parse_farm(input) {
         Ok(v) => v,
         Err(e) => {
             println!("couldn't parse farm: {e}");
             return None;
         }
     };
+
     let mut count = 0;
-    for ((y, x), region) in regions {
-        let plot = plots.get(y, x).unwrap();
-        let price = region.area * region.perimeter;
-        println!(
-            "A region of {} at ({},{}) with price {} * {} = {}",
-            plot.plant, x, y, region.area, region.perimeter, price
-        );
-        count += price;
+    for y in 0..plots.row_len() {
+        for x in 0..plots.column_len() {
+            let plot = plots.get(y, x)?;
+            if plot.processed {
+                continue;
+            }
+            let plant = plot.plant;
+            let (area, perimiter) = find_friends(&mut plots, plant, x, y);
+            count += area * perimiter;
+        }
     }
     Some(count)
 }
@@ -32,12 +33,7 @@ pub fn part_two(_input: &str) -> Option<u32> {
 #[derive(Clone)]
 struct Plot {
     plant: char,
-    region: (usize, usize),
-}
-
-struct Region {
-    area: u32,
-    perimeter: u32,
+    processed: bool,
 }
 
 #[derive(Clone, Debug)]
@@ -60,121 +56,59 @@ impl Direction {
     }
 
     fn all() -> Vec<Direction> {
-        vec![Direction::East, Direction::South, Direction::West, Direction::North]
+        vec![
+            Direction::East,
+            Direction::South,
+            Direction::West,
+            Direction::North,
+        ]
     }
 }
 
-fn parse_farm(input: &str) -> Result<(Array2D<Plot>, HashMap<(usize, usize), Region>), Error> {
+fn parse_farm(input: &str) -> Result<Array2D<Plot>, Error> {
     let mut rows = vec![];
     for line in input.lines() {
         let mut row = vec![];
         for c in line.chars() {
             row.push(Plot {
                 plant: c,
-                region: (0, 0),
+                processed: false,
             });
         }
         rows.push(row);
     }
-    let mut plots: Array2D<Plot> = Array2D::from_rows(&rows)?;
-    let mut regions: HashMap<(usize, usize), Region> = HashMap::new();
-    for y in 0..plots.row_len() {
-        for x in 0..plots.column_len() {
-            let mut region_index = (y, x);
-            let mut region_index_up = None;
-            let mut region_index_left = None;
-            let mut region_index_merge = None;
-            let mut region = Region {
-                area: 1,
-                perimeter: 0,
-            };
-            let mut region_up = Region {
-                area: 0,
-                perimeter: 0,
-            };
-            let mut region_left = Region {
-                area: 0,
-                perimeter: 0,
-            };
-            let plot_current = plots.get(y, x).unwrap();
-
-            let mut found_up = false;
-            if y == 0 {
-                region.perimeter += 1;
-            } else {
-                let plot_up = plots.get(y - 1, x).unwrap();
-                if plot_up.plant == plot_current.plant {
-                    region_index = plot_up.region;
-                    found_up = true;
-                } else {
-                    region_index_up = Some(plot_up.region);
-                    region.perimeter += 1;
-                    region_up.perimeter += 1;
-                }
-            }
-            if x == 0 {
-                region.perimeter += 1;
-            } else {
-                let plot_left = plots.get(y, x - 1).unwrap();
-                if plot_left.plant == plot_current.plant {
-                    region_index = plot_left.region;
-                    if found_up {
-                        region_index_merge = Some(plot_left.region);
-                    }
-                } else {
-                    region_index_left = Some(plot_left.region);
-                    region.perimeter += 1;
-                    region_left.perimeter += 1;
-                }
-            }
-            if y == plots.row_len() - 1 {
-                region.perimeter += 1;
-            }
-            if x == plots.column_len() - 1 {
-                region.perimeter += 1;
-            }
-
-            let plot_current = plots.get_mut(y, x).unwrap();
-            plot_current.region = region_index;
-            regions
-                .entry(region_index)
-                .and_modify(|r| r.area += region.area)
-                .and_modify(|r| r.perimeter += region.perimeter)
-                .or_insert(region);
-            if let Some(region_index_up) = region_index_up {
-                regions
-                    .entry(region_index_up)
-                    .and_modify(|r| r.area += region_up.area)
-                    .and_modify(|r| r.perimeter += region_up.perimeter)
-                    .or_insert(region_up);
-            }
-            if let Some(region_index_left) = region_index_left {
-                regions
-                    .entry(region_index_left)
-                    .and_modify(|r| r.area += region_left.area)
-                    .and_modify(|r| r.perimeter += region_left.perimeter)
-                    .or_insert(region_left);
-            }
-            if let Some(region_index_discard) = region_index_merge {
-                let region_merge = regions.get(&region_index_discard).unwrap();
-                let area = region_merge.area;
-                let perimeter = region_merge.perimeter;
-                regions
-                    .entry(region_index)
-                    .and_modify(|r| r.area += area)
-                    .and_modify(|r| r.perimeter += perimeter);
-                regions.remove(&region_index_discard).unwrap();
-            }
-        }
-    }
-
-    Ok((plots, regions))
+    Array2D::from_rows(&rows)
 }
 
-fn find_friends(plots: Array2D<Plot>) -> (u32, u32) {
-    let mut area = 0;
-    let mut perimeter = 0;
-    for
+fn find_friends(plots: &mut Array2D<Plot>, friend: char, x: usize, y: usize) -> (u32, u32) {
+    let plot = match plots.get_mut(y, x) {
+        Some(v) => v,
+        None => return (0, 1),
+    };
+    if plot.plant != friend {
+        return (0, 1);
+    }
+    // stop if we've already taken this plot into account
+    if plot.processed {
+        return (0, 0);
+    }
+
+    plot.processed = true;
+    let mut area = 1;
+    let mut perimiter = 0;
+    for d in Direction::all() {
+        let (next_y, next_x) = match d.step(y, x) {
+            Some(v) => v,
+            None => {
+                perimiter += 1;
+                continue;
+            }
+        };
+        let (a, p) = find_friends(plots, friend, next_x, next_y);
+        area += a;
+        perimiter += p;
+    }
+    (area, perimiter)
 }
 
 #[cfg(test)]
